3f6cb4b07112402540b18a03258cf84ab444b16e
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2024-10-08 18:12:58 +0200

    [UPDATE] Create the data request function on the service.mjs file

---
+++ service.mjs
@@ -0,0 +1,13 @@
+
+
+const getData = async () => {
+    try {
+
+        const result = await fetch('https://raw.githubusercontent.com/zsiciarz/skyrim-alchemy-toolbox/master/data/ingredients.json');
+        return (result.json());
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export {getData};
